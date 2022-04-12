f68596b72e31a65f4aa9efacc87e4b6ebde172f9
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-04-12 17:35:18 +0200

    Update svm.py

--- svm.py
+++ svm.py
@@ -5,18 +5,16 @@ from sklearn.model_selection import cross_val_score
 
 def run(dataset):
     np.random.shuffle(dataset)
-    classes = dataset[:, 57] 
-    features = dataset[:, :54] 
-    features = TD_IDF(features) 
+    classes = dataset[:, -1]
+    features = dataset[:, :54]
+    features = TD_IDF(features)
     svm("linear", 3, features, classes) #3 default degree  value in SVC, it's ignored in linear and rbf
     svm("poly", 2, features, classes)
     svm("rbf", 3, features, classes)
-    # Normalization of dataset in order to exploit angle
-    norms = np.sqrt(((features+1e-100)**2).sum(axis=1, keepdims=True))
-    features_norm = np.where(norms > 0.0, features / norms, 0.)
-    svm("linear", 3, features_norm, classes)
-    svm("poly", 2, features_norm, classes)
-    svm("rbf", 3, features_norm, classes)
+    features = normalize(features)
+    svm("linear", 3, features, classes)
+    svm("poly", 2, features, classes)
+    svm("rbf", 3, features, classes)
     
 
 def view(result, kernel):
@@ -29,7 +27,7 @@ def view(result, kernel):
 
 def svm(kernel, degree, features, classes):
     classifier = SVC(kernel=kernel, degree=degree) 
-    result = cross_val_score(classifier, features, classes, n_jobs=-1) 
+    result = cross_val_score(classifier, features, classes, cv=10, n_jobs=-1) 
     view(result, classifier.kernel)
 
 #TF/IDF representation.
@@ -38,3 +36,7 @@ def TD_IDF(features):
     ndoc = features.shape[0]
     idf = np.log10(ndoc/(features != 0).sum(0))
     return tf*idf
+
+def normalize(features):
+    norms = np.sqrt(((features+1e-100)**2).sum(axis=1, keepdims=True))
+    return np.where(norms > 0.0, features / norms, 0.)
