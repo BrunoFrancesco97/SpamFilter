0f28ce132a42671f4de3e773fddf78cda5fcab57
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-04-12 17:35:52 +0200

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import cross_val_score
 
 def run(data, k):
     np.random.shuffle(data)
-    classes = data[:, -1]
-    features = data[:, :54]
+    classes = data[:, -1] # Classification for each email in the dataset
+    features = data[:, :54] # Features for each email in the dataset
     knn(k, features, classes)
 
 def knn(k, features, classes):
@@ -19,4 +19,5 @@ def view(results):
     print("Minimum accuracy: "+str(results.min()))
     print("Maximum accuracy: "+str(results.max()))
     print("Accuracy mean: "+str(results.mean()))
+    print("Accuracy variance: "+str(results.var()))
     print("***********")
