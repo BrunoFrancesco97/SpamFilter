9dceaf8252f7ba73c9f2e667fa497dd6228695bf
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-04-16 18:05:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import numpy as np
 import svm as svm
 import knn as knn
-
+import nb as nb
 
 filedata = "spambase/spambase.data"
 file = open(filedata, "r")
 dataset = np.loadtxt(file, delimiter = ",")
-
 print("SVM Classifier:")
 svm.run(dataset)
-
-# Run Naive Bayes Classifier on dataset with 10-fold cross validation
-#k_folds = 10
-#NB.run(dataset, k_folds)
-
+print("Naive-Bayes Classifier:")
+nb.nb(dataset)
 print("K-Nearest Neighbors Classifier:")
 knn.run(dataset, 5)
